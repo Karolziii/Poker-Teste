8c5274f72ac80e02930419e363f2859e9fc8f041
Karolziii <karolinedutra22@gmail.com>
2024-05-13 03:45:38 -0300

    agora sim 08

--- poker_base-M2/poker/obj/Debug/net8.0/poker.AssemblyInfo.cs
+++ poker_base-M2/poker/obj/Debug/net8.0/poker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("poker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34726ac4ce2b7a743be26be88c88e58cabacedf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425fd578564e6a653ba9869ed98ec73d589a7519")]
 [assembly: System.Reflection.AssemblyProductAttribute("poker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("poker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
