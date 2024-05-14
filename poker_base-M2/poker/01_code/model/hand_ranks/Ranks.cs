a1e7e0c8ae7057d68ce1dc419314969960241705
Karolziii <karolinedutra22@gmail.com>
2024-05-14 19:40:47 -0300

    Update Ranks.cs

--- poker_base-M2/poker/01_code/model/hand_ranks/Ranks.cs
+++ poker_base-M2/poker/01_code/model/hand_ranks/Ranks.cs
@@ -1,25 +1,27 @@
 namespace poker.code.model.hand_ranks;
 /*
-* Base class, of all Hand Ranks classes.
+* Classe base para Hand Ranks classes.
 */
 public abstract class Ranks
 {
     //---------------------------------------------------------------------------------
-    //states
+    //Estados
     //---------------------------------------------------------------------------------
     protected List<List<Card>> ranks_histo; // histogram
-    protected List<Card> find; // gurada rank encontrado
+    protected List<Card> find; // declara um campo protegido na classe 'find', que é uma lista de objetos do tipo 'Card'.
 
     //---------------------------------------------------------------------------------
-    //behaviors
+    //Comportamentos
     //---------------------------------------------------------------------------------    
-    public Ranks(List<List<Card>> l) // constructor
+    public Ranks(List<List<Card>> l) // Construtor público para a classe Ranks, que recebe uma lista de listas de cartas (l) como parâmetro.
     {
-        ranks_histo = l;
-        find = new List<Card>();
+        ranks_histo = l; // Atribui a lista de listas de cartas 'l' ao campo 'ranks_histo' 
+        find = new List<Card>(); // Inicializa a lista 'find' com uma nova instância de 'List<Card>()', criando uma lista vazia.
+
+
     }
 
-    public List<Card> get_find() // get find list
+    public List<Card> get_find() // método público chamado get_find(), que retorna a lista find de objetos do tipo 'Card'.
     {
         return find;
     }
@@ -27,9 +29,9 @@ public abstract class Ranks
     //verifica se na lista passada existe uma Card com owner == 1
     protected bool check_woner(List<Card> l)
     {
-        foreach (var c in l)
+        foreach (var c in l) // Itera sobre cada carta c na lista l.
         {
-            if (c.owner == 1)
+            if (c.owner == 1) // Verifica se o proprietário da carta c é igual a 1.
                 return true;
         }
         return false;
@@ -39,33 +41,36 @@ public abstract class Ranks
     //com as duas cartas da pessoa
     protected List<Card> copy_woner()
     {
-        List<Card> list = new List<Card>();
+        List<Card> list = new List<Card>(); //Declaração e inicialização de uma nova lista de cartas chamada 'list'.
 
         foreach (var l in ranks_histo)
         {
-            foreach (var c in l)
+            foreach (var c in l) //  Verifica se o proprietário da carta c é igual a 1.
             {
                 if(c.owner == 1)
-                    list.Add(new Card (c));
+                    list.Add(new Card (c)); // Se o proprietário da carta for igual a 1, cria uma nova instância de 'Card' com base na carta 'c' e a adiciona à lista 'list'.
             }
         }
         return list;
     }
         
-    //Retorna uma copia (valores nao referencia) da lista
-    //passada por parametro
-    protected List<Card> value_copy (List<Card> l)
+    //Retorna uma cópia (valores nao referência) da lista
+    //passada por parâmetro
+    protected List<Card> value_copy (List<Card> l) // Recebe uma lista de cartas l como parâmetro e retorna uma lista de cartas.
     {
+        //// Inicializa uma nova lista de cartas chamada list_copy.
         List<Card> list_copy = new List<Card>();
-    
+
+        //// Loop que itera sobre cada carta c na lista l.
         foreach (var c in l)
         {
+            // Adiciona uma nova instância de Card, que é uma cópia da carta c, à lista list_copy.
             list_copy.Add(new Card(c));
         }
         return list_copy;
     }
         
-    //metodo abstrato, toda classe que herda precisa 
-    //implementar sua versao
+    //método abstrato, toda classe que herda precisa 
+    //implementar sua versão
     public abstract bool check ();
 }
