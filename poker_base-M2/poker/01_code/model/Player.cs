2d6c54df7b9adedc5838400c7da3e26090054022
Karolziii <karolinedutra22@gmail.com>
2024-05-14 19:20:39 -0300

    Update Player.cs

--- poker_base-M2/poker/01_code/model/Player.cs
+++ poker_base-M2/poker/01_code/model/Player.cs
@@ -1,13 +1,13 @@
-//m4rc3lo - 2024-04-23
-// comentar toda a classe
 namespace poker.code.model;
+
+//Declaração de uma classe chamada Player que herda da classe Person.
 public class Player : Person
 {
     // Campo privado específico da classe
-    private int playerNumber;
+    private int playerNumber; // Declaração de um campo privado do tipo int chamado playerNumber, que armazena o número do jogador.
 
     // Construtor
-    public Player(string name, int age, int playerNumber)
+    public Player(string name, int age, int playerNumber) // Declaração de um construtor para a classe Player que recebe três argumentos, string 'name', um inteiro 'age' e um inteiro 'playerNumber'.
         : base(name, age)
     {
         this.playerNumber = playerNumber;
