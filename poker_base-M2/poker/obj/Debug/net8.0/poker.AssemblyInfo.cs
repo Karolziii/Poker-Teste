c66ab904bd8d9849d6be67a0b4bfb8971c1ac74a
Lucas Tizon <lucas_gioti@hotmail.com>
2024-05-14 23:33:23 -0300

    ean

--- poker_base-M2/poker/obj/Debug/net8.0/poker.AssemblyInfo.cs
+++ poker_base-M2/poker/obj/Debug/net8.0/poker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("poker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca0f1a72d3fcd423fc7413b9f0bcc2bffc9248b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b940bd08ee4def839b61a39f036b3c31ef61ca18")]
 [assembly: System.Reflection.AssemblyProductAttribute("poker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("poker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
