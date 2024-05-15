0eb37f499a8aea8d46438aec51878eaafefb3ba3
Karolziii <karolinedutra22@gmail.com>
2024-05-15 01:27:20 -0300

    comentarios input

--- poker_base-M2/poker/01_code/controller/Input.cs
+++ poker_base-M2/poker/01_code/controller/Input.cs
@@ -3,7 +3,7 @@ using poker.code.view;
 namespace poker.code.controller;
 
 /*
-Classe para lidar com entradas do usuário
+Método para ler uma opção do teclado
 */
 public class Input
 {
@@ -18,12 +18,16 @@ public class Input
         int opt = 0;
         while (true)
         {
+            // Tenta converter a entrada do usuário para um número inteiro
             Int32.TryParse(Console.ReadLine(), out opt);
+            // Se o número estiver dentro do intervalo desejado, sai do loop
             if(opt >= 1 && opt <= 2)
                 break;
+            // Caso contrário, mostra uma mensagem de erro e continua pedindo uma entrada válida
             else
                 ShowText.print_text("Valid options: 1 or 2");
         }
+        // Retorna a opção válida
         return opt;
     }
 
@@ -32,6 +36,7 @@ public class Input
     */
     public static string get_text()
     {
+        // Lê e retorna o texto digitado pelo usuário
         return Console.ReadLine()!;
     }
 }
